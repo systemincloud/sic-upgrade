d1849cd78f639be6a65e33b45de55c981b9c5fed
mji <marek.jagielski@gmail.com>
2015-06-13 11:57:52 +0200

    upgrade to 0.7.0

---
+++ com.systemincloud.modeler.upgrade.v0_6_1.to.v0_7_0/src/main/java/com/systemincloud/modeler/upgrade/v0_6_1/to/v0_7_0/Execute.java
@@ -0,0 +1,40 @@
+package com.systemincloud.modeler.upgrade.v0_6_1.to.v0_7_0;
+
+import java.io.File;
+
+import javax.xml.transform.TransformerException;
+
+import com.systemincloud.modeler.upgrade.common.AbstractExecute;
+
+public class Execute extends AbstractExecute {
+
+	@Override
+	public boolean execute(File file) {
+		String xml;
+		try {
+			xml = readFile(file);
+			xml = super.executeOnSic(xml);
+			
+			//
+			//
+			//
+			super.writeFile(file, xml);
+		} catch (Exception e) { return false; }
+		return true;
+	}
+	
+	@Override
+	public boolean execute(String root) {
+		return super.executeOnRoot(root);
+	}
+
+	@Override
+	protected String executeOnPom(String pom) throws TransformerException {
+		return pom;
+	}
+
+	@Override
+	protected String getVersion() {
+		return "0.7.0";
+	}
+}
