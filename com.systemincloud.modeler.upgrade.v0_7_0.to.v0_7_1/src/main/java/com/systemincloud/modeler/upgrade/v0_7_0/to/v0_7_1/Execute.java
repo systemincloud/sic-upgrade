8b49d1aebfc6452106746d8714e4c100c440d59d
mji <marek.jagielski@gmail.com>
2015-07-12 15:16:49 +0200

    upgrade to 0.7.1

---
+++ com.systemincloud.modeler.upgrade.v0_7_0.to.v0_7_1/src/main/java/com/systemincloud/modeler/upgrade/v0_7_0/to/v0_7_1/Execute.java
@@ -0,0 +1,38 @@
+package com.systemincloud.modeler.upgrade.v0_7_0.to.v0_7_1;
+
+import java.io.File;
+
+import com.systemincloud.modeler.upgrade.common.AbstractExecute;
+
+public class Execute extends AbstractExecute {
+
+    @Override
+    public boolean execute(File file) {
+        String xml;
+        try {
+            xml = readFile(file);
+            xml = super.executeOnSic(xml);
+
+            //
+            //
+            //
+            super.writeFile(file, xml);
+        } catch (Exception e) { return false; }
+        return true;
+    }
+
+    @Override
+    public boolean execute(String root) {
+        return super.executeOnRoot(root);
+    }
+
+    @Override
+    protected String getVersion() {
+        return "0.7.1";
+    }
+
+    @Override
+    protected String executeOnPom(String pom) {
+        return pom;
+    }
+}
